341035ba1f554f3105f1537047d7b5b3fda71241
knighthat <knightchanelgaming@gmail.com>
2024-04-02 21:56:54 -0500

    implemented new VideoPreviewCard class

--- src/main/java/me/knighthat/api/v1/trending/MainPageController.java
+++ src/main/java/me/knighthat/api/v1/trending/MainPageController.java
@@ -1,10 +1,8 @@
 package me.knighthat.api.v1.trending;
 
-import com.google.api.services.youtube.model.Channel;
-import com.google.api.services.youtube.model.ChannelSnippet;
 import com.google.api.services.youtube.model.Video;
-import com.google.api.services.youtube.model.VideoSnippet;
 import me.knighthat.api.v1.instances.PreviewCard;
+import me.knighthat.api.v1.instances.VideoPreviewCard;
 import me.knighthat.api.youtube.YoutubeAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,28 +16,11 @@ import java.util.List;
 public class MainPageController {
 
     @GetMapping( "/popular" )
-    @CrossOrigin
     public ResponseEntity<List<PreviewCard>> popular( @RequestParam( required = false ) int max, @RequestParam( required = false ) String region ) {
         List<PreviewCard> cards = new ArrayList<>( max );
 
-        for (Video video : YoutubeAPI.getPopular( max, region )) {
-            VideoSnippet vSnippet = video.getSnippet();
-            Channel channel = YoutubeAPI.channel( vSnippet.getChannelId(), "snippet" );
-            ChannelSnippet cSnippet = channel.getSnippet();
-
-            cards.add( new PreviewCard(
-                    video.getId(),
-                    vSnippet.getThumbnails().getHigh().getUrl(),
-                    convertDuration( video.getContentDetails().getDuration() ),
-                    vSnippet.getLocalized().getTitle(),
-                    cSnippet.getCustomUrl(),
-                    cSnippet.getThumbnails().getDefault().getUrl(),
-                    cSnippet.getTitle(),
-                    video.getStatistics().getLikeCount(),
-                    video.getStatistics().getViewCount(),
-                    vSnippet.getPublishedAt()
-            ) );
-        }
+        for (Video video : YoutubeAPI.getPopular( max, region ))
+            cards.add( new VideoPreviewCard( video ) );
 
         return ResponseEntity.ok( cards );
     }
