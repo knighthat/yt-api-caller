5c8f7e51c1439f66688d6728ca61c6d43ed88edf
knighthat <knightchanelgaming@gmail.com>
2024-04-05 18:02:52 -0500

    endpoint to retrieve VideoPlayer

---
+++ src/main/java/me/knighthat/api/v1/VideoPlayerController.java
@@ -0,0 +1,51 @@
+package me.knighthat.api.v1;
+
+import com.google.api.services.youtube.model.Video;
+import me.knighthat.api.v1.instances.VideoPlayer;
+import me.knighthat.api.youtube.YoutubeAPI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping( "/v1" )
+public class VideoPlayerController {
+
+    private VideoPlayer fetchVideoPlayer( String id ) {
+        try {
+            Video video = YoutubeAPI.getService()
+                                    .videos()
+                                    .list( "id,snippet,statistics" )
+                                    .setKey( YoutubeAPI.API_KEY )
+                                    .setId( id )
+                                    .execute()
+                                    .getItems()
+                                    .get( 0 );
+
+            return new VideoPlayer( video );
+        } catch ( IOException e ) {
+            System.err.println( "Error occurs while creating VideoPlayer id: " + id );
+            e.printStackTrace();
+
+            return VideoPlayer.DUMMY;
+        }
+    }
+
+    @GetMapping( value = "/player/{id}" )
+    @CrossOrigin
+    public ResponseEntity<VideoPlayer> videoPlayer( @PathVariable( "id" ) String id ) {
+        ResponseEntity.BodyBuilder builder;
+
+        if ( !id.isEmpty() ) {
+            VideoPlayer player = fetchVideoPlayer( id );
+            if ( player == VideoPlayer.DUMMY )
+                builder = ResponseEntity.internalServerError();
+            else
+                return ResponseEntity.ok( player );
+        } else
+            builder = ResponseEntity.badRequest();
+
+        return builder.build();
+    }
+}
