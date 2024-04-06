6b5f053ae424261102c5c0306ab99159e23fb901
knighthat <knightchanelgaming@gmail.com>
2024-04-06 12:06:24 -0500

    verify env variable & start YouTubeAPI v2

--- src/main/java/me/knighthat/api/KnighthatMeApplication.java
+++ src/main/java/me/knighthat/api/KnighthatMeApplication.java
@@ -1,7 +1,11 @@
 package me.knighthat.api;
 
+import me.knighthat.api.v2.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @SpringBootApplication
 public class KnighthatMeApplication {
@@ -10,5 +14,25 @@ public class KnighthatMeApplication {
         SpringApplication.run( KnighthatMeApplication.class, args );
 
         me.knighthat.api.v1.YoutubeAPI.init();
+
+        /* Startup stage - Load environment variables & start YouTubeAPI */
+        try {
+            me.knighthat.api.v2.Env.verify();
+        } catch ( Exception e ) {
+            Logger.severe( "Error occurs during startup!" );
+            Logger.severe( "Reason: " + e.getMessage() );
+
+            System.exit( 1 );
+        }
+
+        /* Running stage - initialize YouTube Service */
+        try {
+            me.knighthat.api.v2.YoutubeAPI.init();
+        } catch ( GeneralSecurityException | IOException e ) {
+            Logger.severe( "Error occurs while setting up Youtube Service!" );
+            Logger.severe( "Reason: " + e.getMessage() );
+
+            System.exit( 1 );
+        }
     }
 }
