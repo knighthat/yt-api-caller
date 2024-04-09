481e910de4d3b4ae0376e80418f1d554e6ceb5a2
knighthat <knightchanelgaming@gmail.com>
2024-04-09 18:22:14 -0500

    allow request creation with static methods

--- src/main/java/me/knighthat/api/v2/error/BadRequestTemplate.java
+++ src/main/java/me/knighthat/api/v2/error/BadRequestTemplate.java
@@ -5,7 +5,13 @@ import org.springframework.http.ResponseEntity;
 
 public final class BadRequestTemplate extends AbstractErrorTemplate {
 
+    public static @NotNull ResponseEntity<BadRequestTemplate> body( @NotNull String reason ) {
+        return new BadRequestTemplate( reason, "" ).makeResponse();
+    }
+
     BadRequestTemplate( @NotNull Throwable throwable ) { super( throwable ); }
+
+    BadRequestTemplate( @NotNull String reason, @NotNull String stackTrace ) { super( reason, stackTrace ); }
 
     @Override
     @NotNull ResponseEntity<BadRequestTemplate> makeResponse() {
--- src/main/java/me/knighthat/api/v2/error/ServerErrorTemplate.java
+++ src/main/java/me/knighthat/api/v2/error/ServerErrorTemplate.java
@@ -5,7 +5,13 @@ import org.springframework.http.ResponseEntity;
 
 public class ServerErrorTemplate extends AbstractErrorTemplate {
 
+    public static @NotNull ResponseEntity<ServerErrorTemplate> body( @NotNull String reason ) {
+        return new ServerErrorTemplate( reason, "" ).makeResponse();
+    }
+
     ServerErrorTemplate( @NotNull Throwable throwable ) { super( throwable ); }
+
+    ServerErrorTemplate( @NotNull String reason, @NotNull String stackTrace ) { super( reason, stackTrace ); }
 
     @Override
     @NotNull ResponseEntity<ServerErrorTemplate> makeResponse() {
