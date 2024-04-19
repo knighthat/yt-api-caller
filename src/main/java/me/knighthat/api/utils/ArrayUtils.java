7a1480f6d11eb3e45ef7dc6c79fa019b1139bc2f
knighthat <knightchanelgaming@gmail.com>
2024-04-18 20:40:22 -0500

    convert string array to string with delimiter and range

--- src/main/java/me/knighthat/api/utils/ArrayUtils.java
+++ src/main/java/me/knighthat/api/utils/ArrayUtils.java
@@ -1,5 +1,6 @@
 package me.knighthat.api.utils;
 
+import org.checkerframework.checker.index.qual.Positive;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -20,6 +21,23 @@ public class ArrayUtils {
         for (StackTraceElement e : arr)
             if ( e != null )
                 builder.add( e.toString() );
+
+        return builder.toString();
+    }
+
+    @Contract(
+            value = "null,_,_ -> null",
+            pure = true
+    )
+    public static @Nullable String toString( @Nullable String[] arr, @NotNull String delimiter, @Positive int from ) {
+        if ( arr == null )
+            return null;
+
+        StringJoiner builder = new StringJoiner( delimiter );
+        for (int i = 0 ; i < arr.length ; i++) {
+            if ( i >= from )
+                builder.add( arr[i] );
+        }
 
         return builder.toString();
     }
